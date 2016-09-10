e80a37e17c725fc8280f36c5eba02cb818cdfd57
Lucas Jovencio <lucasjovencio@outlook.com.br>
2016-09-10 15:52:54 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 	sudo mv dbs-neo4j.sh /usr/bin/
-	sudo mv neo4j.desktop /usr/share/applications/
+	sudo mv Menu-Neo4j.desktop /usr/share/applications/
 	sudo mv icon-neo4j-ss.png /usr/share/applications/
 	sudo chmod +x /usr/bin/dbs-neo4j.sh
-	sudo chmod +x /usr/share/applications/neo4j.desktop
-	/usr/share/applications/neo4j.desktop
+	sudo chmod +x /usr/share/applications/Menu-Neo4j.desktop
+	/usr/share/applications/Menu-Neo4j.desktop
